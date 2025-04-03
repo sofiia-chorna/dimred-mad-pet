43eb377f351ec361442edbac402192807508c4a5
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-04-03 13:56:58 +0200

    Add consts

---
+++ src/utils/consts.py
@@ -0,0 +1,12 @@
+import torch
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+SPLITS = ["train", "val", "test"]
+BATCH_SIZE = 64
+
+DATASET_FOLDER = "v1.5"
+TYPE = "cleaned-pbc"
+OUTPUT_FOLDER = f"{DATASET_FOLDER}_{TYPE}_merged"
+
+LFF_OUTPUT_FOLDER = f"{DATASET_FOLDER}_cleaned-pbc_llfs"
