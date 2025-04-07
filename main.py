057731a40e5e1eef8ff2632d2898070a9788b6a4
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-04-07 11:06:14 +0200

    Fix order of params

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
 def get_llfs():
     model = NativePET.load_checkpoint("pet-mad-latest.ckpt").eval().to(DEVICE)
     
-    run_get_llfs(model, LFF_OUTPUT_FOLDER, DATASET_FOLDER, TYPE)
+    run_get_llfs(model, DATASET_FOLDER, LFF_OUTPUT_FOLDER, TYPE)
 
 if __name__ == "__main__":
     main()
