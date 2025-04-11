fe2a9d19908b99530a3039fd5622cbbc6f3e7b80
Sofiia Chorna <sofiia.chorna@epfl.ch>
2025-04-11 15:03:16 +0200

    Subsets const

--- src/utils/consts.py
+++ src/utils/consts.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -9,4 +11,15 @@ DATASET_FOLDER = "data/v1.5"
 TYPE = "cleaned-pbc"
 OUTPUT_FOLDER = f"{DATASET_FOLDER}_{TYPE}_merged"
 
+PROJECTION_FOLDER = "data/atom_smap"
+
 LFF_OUTPUT_FOLDER = f"{DATASET_FOLDER}_{TYPE}_llfs"
+
+
+def get_subsets(path="data/v1.5_cleaned-pbc_llfs"):
+    subsets = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    subsets.sort()
+    return subsets
+
+
+SUBSETS = get_subsets()
