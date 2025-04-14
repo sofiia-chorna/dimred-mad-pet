437dfd5fc1b5f655279e1efcdc12b84cb048ebd4
Sofiia Chorna <sofiia.chorna@epfl.ch>
2025-04-14 08:55:24 +0200

    Fix predict function

--- src/train/predict.py
+++ src/train/predict.py
@@ -2,23 +2,29 @@ import os
 
 import torch
 
-from src.utils.consts import LLFS_OUTPUT_FOLDER
+from src.utils.consts import LFF_OUTPUT_FOLDER, DEVICE
 
 
-def predict(subset_models_dict, split="test"):
+def run_predict(subset_models_dict, split="test"):
     res = {}
 
-    for subset_name, output in subset_models_dict.items():
-        mlp_model = output.mlp_model
-        lin_model = output.lin_model
+    for subset_name, items in subset_models_dict.items():
+        (
+            mlp_model,
+            lin_model,
+            X_mean,
+            X_std,
+            y_mean,
+            y_std,
+        ) = items
 
-        x_path = os.path.join(LLFS_OUTPUT_FOLDER, subset_name, f"{split}.pt")
+        x_path = os.path.join(LFF_OUTPUT_FOLDER, subset_name, f"{split}.pt")
+        X = torch.load(x_path, map_location="cpu", weights_only=False).float()
 
-        X = torch.load(x_path, map_location="cpu").float()
         X_np = X.numpy()
 
         # standardize
-        X_std_np = (X_np - output.X_mean) / output.X_std
+        X_std_np = (X_np - X_mean) / X_std
         X_std_tensor = torch.tensor(X_std_np, dtype=torch.float32)
 
         # predict
@@ -27,7 +33,7 @@ def predict(subset_models_dict, split="test"):
             mlp_pred_std = mlp_model(X_std_tensor).numpy()
 
         # unstandardize
-        mlp_pred = mlp_pred_std * output.y_std.numpy() + output.y_mean.numpy()
+        mlp_pred = mlp_pred_std * y_std.cpu().numpy() + y_mean.cpu().numpy()
         lin_pred = lin_model.predict(X_std_np)
         res[subset_name] = lin_pred + mlp_pred
 
